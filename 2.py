88df6489e9d45a1031a62963cd14653a591e8e62
TaRa1999R <rezaietara78@gmail.com>
2023-07-14 02:18:20 +0330

    assignment3

--- 2.py
+++ 2.py
@@ -1,11 +1,21 @@
 print (" print an array ")
+print (" 💯 the maximum length of array is 100 💯 ")
 
 import random
 
 n = int ( input (" Please the length of array : "))
 array = []
 
-for i in range ( n ) :
-    array.append ( random.randint ( 0 , 99 ) )
+while n > 100 :
+    print (" 📌 ERROR 📌 ")
+    print (" please enter lower  number ")
+    n = int ( input (" your number must 100 or lower : "))
+
+while len ( array ) < n :
+    x = random.randint ( 0 , 99)
+    if x in array :
+        array.remove ( x )
+
+    array.append ( x )
 
 print (" the array is : ", array )
