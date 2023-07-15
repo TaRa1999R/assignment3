fdc74922e56746fb5b86df289307a872f40775af
TaRa1999R <rezaietara78@gmail.com>
2023-07-16 00:28:50 +0330

    assignment3

--- 6.py
+++ 6.py
@@ -3,23 +3,25 @@ print (" LET's CALCULATE LEAST COMMON MULTIPLER 🤓🧐")
 first = int ( input (" Please enter first number : "))
 second = int ( input (" Please enter second number : "))
 
-first_divisor = []
-second_divisor = []
-all_divisors = []
-LCM = 1
+a_multipler = []
+b_multipler = []
 
-for i in range ( 1 , first + 1 ) :
-    if first % i == 0 :
-        first_divisor.append ( i )
+if first <= second :
+    a = first
+    b = second
 
-print (" First number's divisors are : ", first_divisor )
+elif first > second :
+    a = second
+    b = first
 
-for i in range ( 1 , second + 1 ) :
-    if second % i == 0 :
-        second_divisor.append ( i )
+for i in range ( 1 , b + 1 ) :
+    a_multipler.append ( a * i )
+    b_multipler.append ( b * i )
 
-print (" Second number's divisors are : ", second_divisor )
+    n = i - 1 
 
-
+    if a_multipler[n] in b_multipler :
+        LCM = a_multipler[n]
+        break
 
 print (" The Least Cmmon Multipler of these two numbers is : ", LCM)
