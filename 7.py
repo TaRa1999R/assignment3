f116d994ec304a1f6ec6713fe4546cd2cb69ce9b
TaRa1999R <rezaietara78@gmail.com>
2023-07-16 12:37:37 +0330

    assignment3

--- 7.py
+++ 7.py
@@ -1,1 +1,10 @@
 print (" 🧐 LET'S COUNT THE NIMBER OF WORDS IN A SENTERNCE 🧐 ")
+
+sentence = input (" Please enter your sentence : ")
+words = 1
+
+for i in range ( len ( sentence )) :
+    if sentence[i] == " " :
+        words += 1
+
+print (" Your sentece has ", words, " words 🙂 ")
